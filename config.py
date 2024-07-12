97161640bfbee61e44d86e79f6a2cc2cf3b19342
VISHNU <Vishnuvishva2434@gmail.com>
2024-07-12 11:39:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Rkn_Bots(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
     #start_pic
-    RKN_PIC = os.environ.get("RKN_PIC", "https://telegra.ph/file/21a8e96b45cd6ac4d3da6.jpg")
+    RKN_PIC = os.environ.get("RKN_PIC", "")
 
 
     # wes response configuration
@@ -23,7 +23,7 @@ class Rkn_Bots(object):
     FORCE_SUB = os.environ.get("FORCE_SUB", "") 
     
     # database config
-    DB_NAME = os.environ.get("DB_NAME", "AutoCaption_V05_Bot")     
+    DB_NAME = os.environ.get("DB_NAME", "AutoCaption_Bot")     
     DB_URL = os.environ.get("DB_URL", "")
 
     #caption
