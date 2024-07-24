65f3041ad6d879cfe5959f87b93aae7735b3777c
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-07-24 13:21:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
-# (c) @RknDeveloperr
-# Rkn Developer 
-# Don't Remove Credit 😔
-# Telegram Channel @RknDeveloper & @Rkn_Bots
-# Developer @RknDeveloperr
+import re
+import os
+import time
 
-import re, os, time
 id_pattern = re.compile(r'^.\d+$') 
 
 class Rkn_Bots(object):
@@ -13,32 +10,27 @@ class Rkn_Bots(object):
     API_HASH = os.environ.get("API_HASH", "e6c4b6ee1933711bc4da9d7d17e1eb20")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5918318460:AAHBrhLDzSRtEhC9bG-PLBFdZ3eWnEH7fgU")
 
-    #start_pic
+    # start_pic
     RKN_PIC = os.environ.get("RKN_PIC", "https://telegra.ph/file/462b0bbd3623b54672f75.jpg")
 
-
-    # wes response configuration
+    # web response configuration
     BOT_UPTIME = time.time()
     PORT = int(os.environ.get("PORT", "8080"))
     FORCE_SUB = os.environ.get("FORCE_SUB", "SK_MoviesOffl") 
-    
+
     # database config
     DB_NAME = os.environ.get("DB_NAME", "AutoCaption_Bot")     
     DB_URL = os.environ.get("DB_URL", "mongodb+srv://sankar:sankar@sankar.lldcdsx.mongodb.net/?retryWrites=true&w=majority")
 
-    #caption
-    DEF_CAP = os.environ.get("DEF_CAP",
-                             "<b><a href='telegram.me/Rkn_Bots'>{file_name} Telegram : @RknDeveloper\n\nForward the file before Downloading.</a></b>",
+    # caption
+    DEF_CAP = os.environ.get(
+        "DEF_CAP",
+        "<b><a href='telegram.me/Rkn_Bots'>{file_name} Telegram : @RknDeveloper\n\nForward the file before Downloading.</a></b>"
     )
 
-    #sticker Id
+    # sticker Id
     STICKER_ID = os.environ.get("STICKER_ID", "CAACAgIAAxkBAAELFqBllhB70i13m-woXeIWDXU6BD2j7wAC9gcAAkb7rAR7xdjVOS5ziTQE")
 
-    #admin id
+    # admin id
     ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5821871362').split()]
-    
 
-# Rkn Developer 
-# Don't Remove Credit 😔
-# Telegram Channel @RknDeveloper & @Rkn_Bots
-# Developer @RknDeveloperr
